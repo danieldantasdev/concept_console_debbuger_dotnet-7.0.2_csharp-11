10e3e655d71ed9e38817ed81242da04784548340
lobinhodev <contatolobinhodev@gmail.com>
2023-04-30 23:56:58 -0300

    init: commit initial

---
+++ Debugger/Program.cs
@@ -0,0 +1,11 @@
+﻿var numbers = Console.ReadLine().Split(" ");
+
+foreach (var number in numbers)
+{
+    var numberInt = int.Parse(number);
+    var numberPow = Math.Pow(numberInt, 2);
+    
+    Console.WriteLine($"{numberInt}² = {numberPow}");
+}
+
+Console.ReadKey();
